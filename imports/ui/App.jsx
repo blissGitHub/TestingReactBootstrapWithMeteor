c7310cd6e470bdd7d4bd0128d971cc2a049f36bd
Preston <bliss@Prestons-MacBook-Pro.local>
2017-09-28 16:53:45 +0800

    testing

---
+++ imports/ui/App.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+// App component - represents the whole app
+export default class App extends Component {
+
+
+
+  render() {
+    return (
+      <div>
+        <h1>Testing</h1>
+        <ReactBootstrap.Button /> 
+      </div>
+    );
+  }
+}
